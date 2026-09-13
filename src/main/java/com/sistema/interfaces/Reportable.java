0c701148371843f1901496441e78f8cf1b4253c3
María Alejandra <aleja090511@gmail.com>
2026-09-13 18:50:39 -0500

    Add Reportable interface

---
+++ src/main/java/com/sistema/interfaces/Reportable.java
@@ -0,0 +1,10 @@
+package com.sistema.interfaces;
+
+/**
+ * Interface para objetos que pueden generar reportes.
+ * Implementa Abstracción y Polimorfismo
+ */
+public interface Reportable {
+    String generarReporte();
+    void mostrarReporte();
+}
